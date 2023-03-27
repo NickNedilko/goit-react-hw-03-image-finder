93dde61bccc78766f11ba87b4a02822f72ebcefc
NickNedilko <overlord311089@gmail.com>
2023-03-27 11:47:05 +0300

    refactoring, and add stateMachine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
-import { InfinitySpin } from 'react-loader-spinner';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Api from './services/Api';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Searchbar from './Searchbar/Searchbar';
-import Button from './Button/Button';
 
 class App extends Component {
   state = {
-    images: [],
     search: null,
-    status: 'false',
-    page: 1,
   };
-
-  async componentDidUpdate(_, prevState) {
-    try {
-      const { search, page } = this.state;
-      // this.setState({ status: true });
-      const images = await Api(page, search);
-      const { hits } = images.data;
-      if (prevState.search !== search || prevState.page !== page) {
-        this.setState({
-          images: [...prevState.images, ...hits],
-          // status: false,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    // this.setState({ status: false });
-  }
-
-  // componentDidUpdate(_, prevState) {}
 
   searchInput = message => {
     console.log(message);
@@ -42,21 +16,12 @@ class App extends Component {
     });
   };
 
-  LoadMoreBtn = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-    console.log(this.state.page);
-  };
-
   render() {
-    const { images, status } = this.state;
+    const { search } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.searchInput} />
-        {status && <InfinitySpin width="200" color="#4fa94d" />}
-        <ImageGallery images={this.state.images} />
-        {images.length > 0 && <Button onClick={this.LoadMoreBtn} />}
+        <ImageGallery search={search} />
         <ToastContainer />
       </>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { InfinitySpin } from 'react-loader-spinner';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import Button from '../Button/Button';
+import Api from '../services/Api';
 import css from './ImageGallery.module.css';
 
-const ImageGallery = ({ images }) => {
-  return (
-    <ul className={css.ImageGallery}>
-      {images.map(({ id, webformatURL, tags, largeImageURL }) => {
-        return (
-          <ImageGalleryItem
-            key={id}
-            id={id}
-            src={webformatURL}
-            alt={tags}
-            largeImageURL={largeImageURL}
-          />
-        );
-      })}
-    </ul>
-  );
-};
+class ImageGallery extends PureComponent {
+  state = {
+    images: [],
+    page: 1,
+    status: 'edle',
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { search } = this.props;
+    const { page } = this.state;
+    if (prevProps.search !== search) {
+      this.setState({
+        images: null,
+        page: 1,
+        status: 'pending',
+      });
+    }
+    try {
+      const images = await Api(page, search);
+      const { hits } = images.data;
+      if (prevProps.search !== search || prevState.page !== page) {
+        this.setState({
+          images: [...prevState.images, ...hits],
+          status: 'resolved',
+        });
+      }
+    } catch (error) {
+      console.log('Ошибка от бекЭнда');
+    }
+  }
+
+  LoadMoreBtn = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+      status: 'pending',
+    }));
+    console.log(this.state.page);
+  };
+
+  render() {
+    const { images, status, page } = this.state;
+
+    if (status === 'pending' && page === 1) {
+      return <InfinitySpin width="200" color="#4fa94d" />;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <ul className={css.ImageGallery}>
+          {images &&
+            images.map(({ id, webformatURL, tags, largeImageURL }) => {
+              return (
+                <ImageGalleryItem
+                  key={id}
+                  id={id}
+                  src={webformatURL}
+                  alt={tags}
+                  largeImageURL={largeImageURL}
+                />
+              );
+            })}
+          {status === 'pending' && page > 1 && (
+            <InfinitySpin width="200" color="#4fa94d" />
+          )}
+          {images.length > 0 && <Button onClick={this.LoadMoreBtn} />}
+        </ul>
+      );
+    }
+  }
+}
 
 export default ImageGallery;
